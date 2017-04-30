da4647d3edaae739d4fb72c0b3a70920e677db35
clay richardson <clayrichardson@users.noreply.github.com>
2017-04-30 15:19:07 -0700

    adding new test

--- test/kv/registy_test.exs
+++ test/kv/registy_test.exs
@@ -21,4 +21,11 @@ defmodule KV.RegistyTest do
     KV.Registry.stop(registry)
     assert Process.alive?(registry) == false
   end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
